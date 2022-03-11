dbf97dfb89168563ae84c4a5db135fa99c1311a7
suochantsao <suochantsao@gmail.com>
2022-03-12 01:54:45 +0800

    files settings

---
+++ src/components/layout/MainLayout.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./Navbar";
+
+function MainLayout(props) {
+  return (
+    <div className="">
+      <Navbar />
+    </div>
+  );
+}
+
+export default MainLayout;
---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,16 @@
+function Navbar() {
+  return (
+    <header className="flex border-current border-2">
+      <h1>FinGarage</h1>
+      <ul className="flex">
+        <li>文章專區</li>
+        <li>討論專區</li>
+        <li>點數排行</li>
+        <li>服務內容</li>
+        <li>註冊和登入</li>
+      </ul>
+    </header>
+  );
+}
+
+export default Navbar;
