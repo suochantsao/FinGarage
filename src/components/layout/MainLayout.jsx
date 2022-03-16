1100441a245d750d2407a8e0163cf9f82668e59c
suochantsao <suochantsao@gmail.com>
2022-03-16 23:13:47 +0800

    [style]Navbar btn hover settings

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -1,13 +1,12 @@
 // Components
 import Navbar from "./Navbar";
-// import banner from "../../images/banner.jpg";
 
 function MainLayout(props) {
   return (
     <div className="">
       <Navbar />
       <div className="bg-home-banner text-white text-center bg-cover pt-60 pb-32 font-NotoSans">
-        <h2 className="main-text text-3xl text-shadow-wh mb-3">
+        <h2 className="main-text text-3xl text-shadow-wh mb-2">
           FinGarage <span className="font-NotoSans">財酷</span>
         </h2>
         <p className="text-shadow-bk">你的財經知識庫</p>
@@ -15,7 +14,6 @@ function MainLayout(props) {
           你想知道的財經知識，盡在 FinGarage 財經知識庫
         </p>
       </div>
-      {/* <img className="h-1/2" src={banner} alt="banner" /> */}
     </div>
   );
 }
--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -4,11 +4,11 @@ function Navbar() {
       <h1 className="main-text text-2xl cursor-pointer">FinGarage</h1>
       <nav>
         <ul className="flex space-x-6 font-NotoSans">
-          <li className="btn">文章專區</li>
-          <li className="btn">討論專區</li>
-          <li className="btn">點數排行</li>
-          <li className="btn">服務內容</li>
-          <li className="btn">註冊和登入</li>
+          <li className="nav-btn btn">精選文章</li>
+          <li className="nav-btn btn">討論專區</li>
+          <li className="nav-btn btn">點數排行</li>
+          <li className="nav-btn btn">關於我們</li>
+          <li className="primary-btn btn">註冊和登入</li>
         </ul>
       </nav>
     </header>
