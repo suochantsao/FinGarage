3f2cba5c7279cb2e0628ef4d7000f79fcb0a5da8
suochantsao <suochantsao@gmail.com>
2022-06-29 02:14:02 +0800

    [LAYOUT]UI Redesigned

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -2,26 +2,31 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <header className="flex justify-between items-center container mx-auto p-5 ">
-      <h1 className="main-text text-2xl cursor-pointer">
-        <Link to="/">FinGarage</Link>
-      </h1>
-      <nav>
-        <ul className="flex space-x-6 font-NotoSans">
-          <li className="nav-btn btn">
-            <Link to="/article">精選文章</Link>
-          </li>
-          <li className="nav-btn btn">
-            <Link to="/forum">討論專區</Link>
-          </li>
-          <li className="nav-btn btn">
-            <Link to="/leaderboard">點數排行</Link>
-          </li>
-          <li className="nav-btn btn">關於我們</li>
-          <li className="primary-btn btn">註冊和登入</li>
-        </ul>
-      </nav>
-    </header>
+    <div className='fixed w-full bg-gradient-to-r from-[#1a1b1e] to-[#414141]'>
+      <header className="flex justify-between items-center container mx-auto p-4">
+        <h1 className="text-2xl">
+          <Link to="/">
+            <img src={window.location.origin + '/images/logo.png'} alt='' />
+          </Link>
+        </h1>
+        <nav>
+          <ul className="flex space-x-6 font-NotoSans pt-1">
+            <li className="nav-btn btn">
+              <Link to="/article">精選文章</Link>
+            </li>
+            <li className="nav-btn btn">
+              <Link to="/forum">討論專區</Link>
+            </li>
+            <li className="nav-btn btn">
+              <Link to="/leaderboard">點數排行</Link>
+            </li>
+            <li className="nav-btn btn">關於我們</li>
+            <li className="primary-btn btn">註冊和登入</li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+   
   );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,21 @@
 function Home() {
   return (
-    <div className="bg-home-banner text-white text-center bg-cover pt-60 pb-32 font-NotoSans">
-      <h2 className="main-text text-3xl text-shadow-wh mb-2">
+    <div className="container mx-auto pt-[121px] py-4 text-[#1a1b1e] text-center bg-cover font-NotoSans home">
+      <ul className='flex'>
+        <li className='w-1/2'>
+          {/* <p>你想知道的財經知識，盡在 FinGarage 財經知識庫</p> */}
+        </li>
+        <li className='h-[460px] overflow-hidden'>
+          <img className='mt-[-50px]' src={window.location.origin + '/images/banner.gif'} alt='' />
+        </li>
+      </ul>
+      {/* <h2 className="main-text text-3xl mb-2">
         FinGarage <span className="font-NotoSans">財酷</span>
       </h2>
       <p className="text-shadow-bk">你的財經知識庫</p>
       <p className="mt-36 text-shadow-bk">
-        你想知道的財經知識，盡在 FinGarage 財經知識庫
-      </p>
+        
+      </p> */}
     </div>
   );
 }
